c7f548cfa788871d5317ee0bf31078836ff2f5ed
emamaria <emascript@gmail.com>
2023-05-05 12:02:14 +0200

    ok starting files and home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home.component';
+import { OneMovieComponent } from './pages/one-movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OneMovieComponent
   ],
   imports: [
     BrowserModule
