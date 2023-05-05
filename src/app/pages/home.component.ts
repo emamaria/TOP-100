1ba0c12e61d12dd2010530557e99516745e1ea2a
emamaria <emascript@gmail.com>
2023-05-05 15:51:03 +0200

    ok getparams

--- src/app/pages/home.component.ts
+++ src/app/pages/home.component.ts
@@ -8,7 +8,11 @@ import { HomeMovie } from '../interfaces/home-movies.interface';
 })
 export class HomeComponent implements OnInit {
 
-  moviesList: HomeMovie[] = [{title: "shawshank redemption", img: "img1", rank:1}, {title: "godfather", img: "img2", rank:2}]
+  moviesList: HomeMovie[] = [
+    {title: "shawshank redemption", img: "img1", rank:1}, 
+    {title: "godfather", img: "img2", rank:2},
+    {title: "taxi driver", img: "img3", rank:3}
+  ]
 
   constructor() { }
 
--- src/app/pages/one-movie.component.ts
+++ src/app/pages/one-movie.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-one-movie',
   templateUrl: './one-movie.component.html',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OneMovieComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activateRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+
+    this.activateRoute.params.subscribe(params => {
+
+      const {movieName}  = params;
+
+      console.log(movieName);
+    })
   }
 
 }
