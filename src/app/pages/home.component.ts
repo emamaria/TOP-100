d4a792218182b037d371ceb17200f61ff6e5e4df
emamaria <emascript@gmail.com>
2023-05-07 12:27:29 +0200

    in case no image

--- src/app/pages/home.component.ts
+++ src/app/pages/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { HomeMovie } from '../interfaces/home-movies.interface';
 import { MoviesService } from '../movies.service';
 
@@ -12,7 +12,7 @@ export class HomeComponent implements OnInit {
   inputValue: string = "";
 
  movie: any = [];
-  
+
   moviesList: HomeMovie[] = [
     
    
@@ -20,19 +20,35 @@ export class HomeComponent implements OnInit {
 
  
   moviesListOriginal: HomeMovie[] = [
-    {title: "shawshank redemption", img: "", rank:1}, 
-    {title: "godfather", img: "", rank:2},
-    {title: "taxi driver", img: "", rank:3},
-    {title: "shawshank redemption", img: "", rank:1}, 
-    {title: "godfather", img: "", rank:2},
-    {title: "taxi driver", img: "", rank:3},
-    {title: "shawshank redemption", img: "", rank:1}, 
-    {title: "godfather", img: "", rank:2},
-    {title: "taxi driver", img: "", rank:3},
-    {title: "shaws", img: "", rank:1}, 
-
     
   ]
+
+
+  // {title: "shawshank redemption", img: "", rank:1}, 
+  //   {title: "godfather", img: "", rank:2},
+  //   {title: "taxi driver", img: "", rank:3},
+  //   {title: "shawshank redemption", img: "", rank:1}, 
+  //   {title: "godfather", img: "", rank:2},
+  //   {title: "taxi driver", img: "", rank:3},
+  //   {title: "shawshank redemption", img: "", rank:1}, 
+  //   {title: "godfather", img: "", rank:2},
+  //   {title: "taxi driver", img: "", rank:3},
+  //   {title: "shaws", img: "", rank:1}, 
+  filmsList(){
+    console.log("cargando peliculas");
+    this.moviesListOriginal = [...this.movieService.homeMovies()]
+     this.moviesList = [...this.movieService.homeMovies()]
+    
+    //  console.log("ejecuto filmsList");
+    //  return this.moviesList
+  }
+
+
+  get Date(){
+    this.filmsList()
+     return new Date().getFullYear()
+  }
+    
 
   
 
@@ -40,6 +56,7 @@ export class HomeComponent implements OnInit {
   constructor(private movieService:MoviesService) {
   
   }
+ 
 
   ngOnInit(): void {
     this.moviesList = this.moviesListOriginal
@@ -48,22 +65,22 @@ export class HomeComponent implements OnInit {
     console.log("cargando home");
   }
 
+ 
+
+
+
   onChange(value:string):any{
     console.log("valor", value);
 
+    
+  
     this.moviesList = this.moviesListOriginal
   
     this.moviesList = this.moviesList.filter( list =>  list.title.toLowerCase().includes(value.toLowerCase())) 
    
   }
 
-  // get filmsList(){
-    
-  //    this.moviesList = [...this.movieService.homeMovies()]
-  //    this.moviesListOriginal = [...this.movieService.homeMovies()]
-  //    console.log("ejecuto filmsList");
-  //    return this.moviesList
-  // }
+ 
    
 
 
